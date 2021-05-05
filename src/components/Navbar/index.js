d5efe1d18e04edde0f9a9aac99791d5d4ffab262
Daniel David <danielalejandro.2000@outlook.com>
2021-05-04 23:07:04 -0500

    smooth scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -11,28 +11,34 @@ import {
   NavBtn,
   NavBtnLink
 } from './NavbarElements';
+import { animateScroll } from 'react-scroll';
 
 const Navbar = ({ toggle }) => {
+
+  const toggleHome = () => {
+    animateScroll.scrollToTop(); 
+  }
+
   return (
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo to="/">Daniel David</NavLogo>
+          <NavLogo to="/" onClick={toggleHome}>Daniel David</NavLogo>
           <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
           <NavMenu>
             <NavItem>
-              <NavLinks to="about">About Me</NavLinks>
+              <NavLinks to="about" smooth={true} duration={500} spy={true} exact='true' >About Me</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="skills">Skills</NavLinks>
+              <NavLinks to="skills" smooth={true} duration={500} spy={true} exact='true' >Skills</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="projects">Projects</NavLinks>
+              <NavLinks to="projects" smooth={true} duration={500} spy={true} exact='true' >Projects</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="contact">Contact Me</NavLinks>
+              <NavLinks to="contact" smooth={true} duration={500} spy={true} exact='true' >Contact Me</NavLinks>
             </NavItem>
           </NavMenu>
           <NavBtn>
